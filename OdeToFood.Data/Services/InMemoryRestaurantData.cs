aaaddcfd0b7d800f7d6c04369a01376f0cbecd13
carlos richter <rrch.cr8@gmail.com>
2022-05-29 23:41:55 -0400

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id=1, Name="Casa de campm", Cuisine= CuisineType.Bolivian },
+                new Restaurant{ Id=2, Name="Solo Pastas", Cuisine= CuisineType.Italian },
+                new Restaurant{ Id=3, Name="Burger King", Cuisine= CuisineType.American }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+
+
+}
